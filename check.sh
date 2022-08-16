e62a7cfc0978cb4d39d497ca21f679353e80750b
Willcock, Stuart <stuartjjwillcock@gmail.com>
2022-08-16 11:59:58 +0100

    Ability to add ca certs in to trust private urls

--- check.sh
+++ check.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
+echo "${CA_BUNDLE}" > "/tmp/ca-bundle.pem"
+export NODE_EXTRA_CA_CERTS="/tmp/ca-bundle.pem"
+
 find "${CHECK_DIR}" -name \*.md -print0 | xargs -0 -n1 markdown-link-check --verbose --config "${CONFIG_PATH}"
